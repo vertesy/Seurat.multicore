7075ca912800ae7cd683d887c2e246453f53afe2
Abel Vertesy <vertesy@users.noreply.github.com>
2019-03-11 10:45:25 +0100

    Multicore Examples for Seurat

--- Seurat.Multicore.Examples.R
+++ Seurat.Multicore.Examples.R
@@ -1,361 +1,135 @@
+# ------------------------------------------------------------------------------------------
+## Seurat.Multicore.Examples.R
+# ------------------------------------------------------------------------------------------
+# DO NOT RUN - these are just code snippets not a ready to go pipeline
 
-# Mon Feb 25 13:18:09 2019 ------------------------------
-# https://satijalab.org/seurat/immune_alignment.html
+# rm(list=ls())
+require(dplyr)
+require(gtools)
+require(cowplot)
+require(stringr)
+require(tibble)
+library(Seurat)
+source ('~/GitHub/TheCorvinas/R/CodeAndRoll.R')
+source(".../Seurat.Functions.R")
+require(MarkdownReportsDev)
+require(doMC)
+registerDoMC(6)
 
+OutDir= "/Users/..."
+setup_MarkdownReports(OutDir = OutDir)
 
-
-# read10x from gzipped and using features.tsv [from SO]------------------------
-read10x <- function(dir) {
-  names <- c("barcodes.tsv", "features.tsv", "matrix.mtx")
-  for (i in 1:length(names)) {
-    R.utils::gunzip(paste0(dir, "/", names[i], ".gz"))
-  }
-  file.copy(paste0(dir, "/features.tsv"), paste0(dir, "/genes.tsv"))
-  mat <- Seurat::Read10X(dir)
-  file.remove(paste0(dir, "/genes.tsv"))
-  for (i in 1:length(names)) {
-    R.utils::gzip(paste0(dir, "/", names[i]))
-  }
-  mat
-}
-
-# Save multiple FeaturePlot from a list of genes on A4 jpeg ------------------------
-multiFeaturePlot.A4 <- function(list.of.genes, object = org, plot.reduction='umap'
-                                , colors=c("grey", "red"), nr.Col=2, nr.Row =4, cex = ceiling(12/(nr.Col*nr.Row))
-                                , gene.min.exp = 'q01', gene.max.exp = 'q99'
-                                , jpeg.res = 225, jpeg.q = 90) {
-  lsG = iterBy.over(1:l(list.of.genes), by=nr.Row*nr.Col)
-  for (i in 1:l(lsG)) { print(i )
-    genes = list.of.genes[lsG[[i]]]
-    plotname = kpp(c(plot.reduction,i, genes, 'jpg' ))
-    
-    jjpegA4(plotname, r = jpeg.res, q = jpeg.q)
-    # try(
-    FeaturePlot(object, features.plot =genes, reduction.use = plot.reduction
-                , nCol = nr.Col, cols.use = colors, no.axes = T, no.legend = F, vector.friendly = T
-                , min.cutoff = gene.min.exp, max.cutoff = gene.max.exp, do.return = F
-                , pt.size = cex)
-    # , silent = F    )
-    try.dev.off()
-  }
-}; 
+p=NULL
+p$min.cells = 10
+p$min.features = 200
+# filtering -----
+p$thr.lp.nGene = 6000
+p$thr.hp.nGene = 1000
+p$thr.lp.mito = 0.15
+p$thr.lp.ribo = 0.3
 
 
-# Save multiple FeatureHeatmaps from a list of genes on A4 jpeg -----------------------
-multiFeatureHeatmap.A4 <- function(list.of.genes, object = org, gene.per.page=5
-                                   , group.cells.by= "batch", plot.reduction='umap'
-                                   , cex = ceiling(3/gene.per.page), sep_scale =T
-                                   , gene.min.exp = 'q1', gene.max.exp = 'q99'
-                                   , jpeg.res = 225, jpeg.q = 90) {
-  lsG = iterBy.over(1:l(list.of.genes), by=gene.per.page)
-  for (i in 1:l(lsG)) { print(i )
-    genes = list.of.genes[lsG[[i]]]
-    plotname = kpp(c("FeatureHeatmap",plot.reduction,i, genes, 'jpg' ))
-    
-    jjpegA4(plotname, r = jpeg.res, q = jpeg.q)
-    try(
-      FeatureHeatmap(object, features.plot =genes , group.by = group.cells.by
-                     , reduction.use = plot.reduction, do.return = F
-                     , sep.scale = sep_scale, min.exp = gene.min.exp, max.exp = gene.max.exp
-                     , pt.size = cex, key.position = "top")
-      , silent = T
-    )
-    try.dev.off()
-  }
-}
-
-# LabelPoint ------------------------
-LabelPoint <- function(plot, genes, exp.mat, adj.x.t = 0, adj.y.t = 0, adj.x.s = 0, 
-                       adj.y.s = 0, text.size = 2.5, segment.size = 0.1) {
-  for (i in genes) {
-    x1 <- exp.mat[i, 1]
-    y1 <- exp.mat[i, 2]
-    plot <- plot + annotate("text", x = x1 + adj.x.t, y = y1 + adj.y.t, 
-                            label = i, size = text.size)
-    plot <- plot + annotate("segment",
-                            x = x1 + adj.x.s, 
-                            xend = x1, 
-                            y = y1 +  adj.y.s, 
-                            yend = y1,
-                            size = segment.size)
-  }
-  return(plot)
-}
-
-#  ------------------------
-LabelUR <- function(plot, genes, exp.mat, adj.u.t = 0.1, adj.r.t = 0.15, adj.u.s = 0.05, 
-                    adj.r.s = 0.05, ...) {
-  return(LabelPoint(
-    plot,
-    genes,
-    exp.mat,
-    adj.y.t = adj.u.t,
-    adj.x.t = adj.r.t,
-    adj.y.s = adj.u.s,
-    adj.x.s = adj.r.s,
-    ...
-  ))
-}
-
-#  ------------------------
-LabelUL <- function(plot, genes, exp.mat, adj.u.t = 0.1, adj.l.t = 0.15, adj.u.s = 0.05, 
-                    adj.l.s = 0.05, ...) {
-  return(LabelPoint(
-    plot,
-    genes,
-    exp.mat,
-    adj.y.t = adj.u.t,
-    adj.x.t = -adj.l.t,
-    adj.y.s = adj.u.s,
-    adj.x.s = -adj.l.s,
-    ...
-  ))
-}
-
-#  ------------------------
-LabelBR <- function(plot, genes, exp.mat, adj.u.t = 0.1, adj.l.t = 0.15, adj.u.s = 0.05, 
-                    adj.l.s = 0.05, ...) {
-  return(LabelPoint(
-    plot,
-    genes,
-    exp.mat,
-    adj.y.t = -adj.u.t,
-    adj.x.t = adj.l.t,
-    adj.y.s = adj.u.s,
-    adj.x.s = adj.l.s,
-    ...
-  ))
-}
+p$'min.pct' = 0.2
+p$'logfc.threshold' = 0.5
 
 
-#  ------------------------
-LabelBL <- function(plot, genes, exp.mat, adj.u.t = 0.1, adj.l.t = 0.15, adj.u.s = 0.05, 
-                    adj.l.s = 0.05, ...) {
-  return(LabelPoint(
-    plot,
-    genes,
-    exp.mat,
-    adj.y.t = -adj.u.t,
-    adj.x.t = -adj.l.t,
-    adj.y.s = adj.u.s,
-    adj.x.s = -adj.l.s,
-    ...
-  ))
+# Load the dataset
+InputDir = ""
+Tag = "filtered_feature_bc_matrix."
+inNames = c(  "A"
+              , "B"
+              , "C"
+              , "D")
+
+# v.project = str_split_fixed(inNames, '\\.', 3)[,3]
+# genotype = str_split_fixed(inNames, '\\.', 4)[,4]
+# batch = as.factor(str_split_fixed(v.project, '', 3)[,2])
+
+
+# read in FILTERED -------------------------------------------------------------------------------------
+if (FirstTime) {
+  ls.input <- ls.Seurat <- list.fromNames(inNames)
+  
+  # Read 10x
+  ls.input <- foreach(i=1:l(inNames)) %dopar% {  
+    read10x(p0(InputDir, Tag, inNames[i])) 
+  }; 
+
+  # Create Seurat
+  ls.Seurat <- foreach(i=1:l(inNames)) %dopar% {
+    CreateSeuratObject(raw.data =  ls.input[[i]], project = v.project[i], 
+                       min.cells = p$min.cells, min.features = p$min.features)
+  }; 
+  
+  # Save Seurat
+  OutputDir = "/Users/..."
+  foreach(i=1:l(inNames)) %dopar% {
+    saveRDS(ls.Seurat[[i]], file = ppp(p0(OutputDir, 'filtered.', inNames[i]), 'min.cells', p$min.cells, 'min.features', p$min.features,"Rds") )
+  }; sayy()
+  
+} else {
+  # Read Seurat  
+  ls.Seurat <- foreach(i=1:l(inNames)) %dopar% {
+    readRDS(file = ppp(p0(InputDir, 'filtered.', inNames[i]), 'min.cells.10.min.features.200.Rds') )
+  }; 
+  
 }
 
 
 
+# Add metadata ------------------------------
 
+batch = as.factor(1:3)
 
-# FeaturePlot with different defaults ------------------------------------------------------------------
-aFeaturePlot <- function (object, features.plot, min.cutoff = 'q1', max.cutoff = 'q99', 
-                          dim.1 = 1, dim.2 = 2, cells.use = NULL, pt.size = 1
-                          , cols.use = c("yellow", "red"), pch.use = 16, overlay = FALSE, do.hover = FALSE, 
-                          data.hover = "ident", do.identify = FALSE, reduction.use = "umap", 
-                          use.imputed = FALSE, nCol = NULL, no.axes = T, no.legend = F, 
-                          coord.fixed = FALSE, dark.theme = FALSE, do.return = FALSE, 
-                          vector.friendly = TRUE, png.file = NULL, png.arguments = c(10, 10, 100)) 
-{
-  cells.use <- SetIfNull(x = cells.use, default = colnames(x = object@data))
-  if (is.null(x = nCol)) {
-    nCol <- 2
-    if (length(x = features.plot) == 1) {
-      nCol <- 1
-    }
-    if (length(x = features.plot) > 6) {
-      nCol <- 3
-    }
-    if (length(x = features.plot) > 9) {
-      nCol <- 4
-    }
-  }
-  num.row <- floor(x = length(x = features.plot)/nCol - 1e-05) + 
-    1
-  if (overlay | do.hover) {
-    num.row <- 1
-    nCol <- 1
-  }
-  par(mfrow = c(num.row, nCol))
-  dim.code <- GetDimReduction(object = object, reduction.type = reduction.use, 
-                              slot = "key")
-  dim.codes <- paste0(dim.code, c(dim.1, dim.2))
-  data.plot <- as.data.frame(GetCellEmbeddings(object = object, 
-                                               reduction.type = reduction.use, dims.use = c(dim.1, dim.2), 
-                                               cells.use = cells.use))
-  x1 <- paste0(dim.code, dim.1)
-  x2 <- paste0(dim.code, dim.2)
-  data.plot$x <- data.plot[, x1]
-  data.plot$y <- data.plot[, x2]
-  data.plot$pt.size <- pt.size
-  names(x = data.plot) <- c("x", "y")
-  data.use <- t(x = FetchData(object = object, vars.all = features.plot, 
-                              cells.use = cells.use, use.imputed = use.imputed))
-  min.cutoff <- mapply(FUN = function(cutoff, feature) {
-    ifelse(test = is.na(x = cutoff), yes = min(data.use[feature, 
-                                                        ]), no = cutoff)
-  }, cutoff = min.cutoff, feature = features.plot)
-  max.cutoff <- mapply(FUN = function(cutoff, feature) {
-    ifelse(test = is.na(x = cutoff), yes = max(data.use[feature, 
-                                                        ]), no = cutoff)
-  }, cutoff = max.cutoff, feature = features.plot)
-  check_lengths = unique(x = vapply(X = list(features.plot, 
-                                             min.cutoff, max.cutoff), FUN = length, FUN.VALUE = numeric(length = 1)))
-  if (length(x = check_lengths) != 1) {
-    stop("There must be the same number of minimum and maximum cuttoffs as there are features")
-  }
-  if (overlay) {
-    pList <- list(BlendPlot(data.use = data.use, features.plot = features.plot, 
-                            data.plot = data.plot, pt.size = pt.size, pch.use = pch.use, 
-                            cols.use = cols.use, dim.codes = dim.codes, min.cutoff = min.cutoff, 
-                            max.cutoff = max.cutoff, coord.fixed = coord.fixed, 
-                            no.axes = no.axes, no.legend = no.legend, dark.theme = dark.theme))
-  }
-  else {
-    pList <- mapply(FUN = SingleFeaturePlot, feature = features.plot, 
-                    min.cutoff = min.cutoff, max.cutoff = max.cutoff, 
-                    coord.fixed = coord.fixed, MoreArgs = list(data.use = data.use, 
-                                                               data.plot = data.plot, pt.size = pt.size, pch.use = pch.use, 
-                                                               cols.use = cols.use, dim.codes = dim.codes, no.axes = no.axes, 
-                                                               no.legend = no.legend, dark.theme = dark.theme, 
-                                                               vector.friendly = vector.friendly, png.file = png.file, 
-                                                               png.arguments = png.arguments), SIMPLIFY = FALSE)
-  }
-  if (do.hover) {
-    if (length(x = pList) != 1) {
-      stop("'do.hover' only works on a single feature or an overlayed FeaturePlot")
-    }
-    if (is.null(x = data.hover)) {
-      features.info <- NULL
-    }
-    else {
-      features.info <- FetchData(object = object, vars.all = data.hover)
-    }
-    return(HoverLocator(plot = pList[[1]], data.plot = data.plot, 
-                        features.info = features.info, dark.theme = dark.theme, 
-                        title = features.plot))
-  }
-  else if (do.identify) {
-    if (length(x = pList) != 1) {
-      stop("'do.identify' only works on a single feature or an overlayed FeaturePlot")
-    }
-    return(FeatureLocator(plot = pList[[1]], data.plot = data.plot, 
-                          dark.theme = dark.theme))
-  }
-  else {
-    print(x = cowplot::plot_grid(plotlist = pList, ncol = nCol))
-  }
-  ResetPar()
-  if (do.return) {
-    return(pList)
-  }
+ls.META<- foreach(i=1:l(ls.Seurat)) %dopar% {
+  META = ls.Seurat[[i]]@meta.data
+  mito.genes <- grep(pattern = "^MT\\.", x = rownames(x = ls.Seurat[[i]]@data), value = TRUE)
+  percent.mito <- Matrix::colSums(ls.Seurat[[i]]@data[mito.genes, ])/Matrix::colSums(ls.Seurat[[i]]@data)
+  
+  ribo.genes <- grep(pattern = "^RPL|^RPS", x = rownames(x = ls.Seurat[[i]]@data), value = TRUE)
+  percent.ribo <- Matrix::colSums(ls.Seurat[[i]]@data[ribo.genes, ])/Matrix::colSums(ls.Seurat[[i]]@data)
+  
+  META$'percent.mito' <- percent.mito
+  META$'percent.ribo' <- percent.ribo
+  META$'log10.nUMI' <- as.named.vector(META[,'nUMI', drop=F])
+  META$'log10.nGenes' <- as.named.vector(META[,'nGene', drop=F])
+  nCells = nrow(META)
+  META$'batch'    <- rep(batch[i], nCells)
+  META
 }
 
+# calculation can take long, and subsettign back only worked in normal for loop AFAIR
+for (i in 1:3) { ls.Seurat[[i]]@meta.data <- ls.META[[i]] }
 
 
-# FeaturePlot version ??? ------------------------------------------------------------------
+# FilterCells ------------------------------
 
-# 
-# FeaturePlot2 <-function (object, features.plot, min.cutoff = NA, max.cutoff = NA, 
-#                          dim.1 = 1, dim.2 = 2, cells.use = NULL, pt.size = 1, 
-#                          cols.use = c("yellow", 
-#                                       "red"), pch.use = 16, overlay = FALSE, do.hover = FALSE, 
-#                          data.hover = "ident", do.identify = FALSE, reduction.use = "tsne", 
-#                          use.imputed = FALSE, nCol = NULL, no.axes = FALSE, no.legend = TRUE, 
-#                          coord.fixed = FALSE, dark.theme = FALSE, do.return = FALSE, 
-#                          vector.friendly = FALSE, png.file = NULL, png.arguments = c(10, 
-#                                                                                      10, 100)) 
-# {
-#   cells.use <- SetIfNull(x = cells.use, default = colnames(x = object@data))
-#   if (is.null(x = nCol)) {
-#     nCol <- 2
-#     if (length(x = features.plot) == 1) {
-#       nCol <- 1
-#     }
-#     if (length(x = features.plot) > 6) {
-#       nCol <- 3
-#     }
-#     if (length(x = features.plot) > 9) {
-#       nCol <- 4
-#     }
-#   }
-#   num.row <- floor(x = length(x = features.plot)/nCol - 1e-05) + 
-#     1
-#   if (overlay | do.hover) {
-#     num.row <- 1
-#     nCol <- 1
-#   }
-#   par(mfrow = c(num.row, nCol))
-#   dim.code <- GetDimReduction(object = object, reduction.type = reduction.use, 
-#                               slot = "key")
-#   dim.codes <- paste0(dim.code, c(dim.1, dim.2))
-#   data.plot <- as.data.frame(GetCellEmbeddings(object = object, 
-#                                                reduction.type = reduction.use, dims.use = c(dim.1, dim.2), 
-#                                                cells.use = cells.use))
-#   x1 <- paste0(dim.code, dim.1)
-#   x2 <- paste0(dim.code, dim.2)
-#   data.plot$x <- data.plot[, x1]
-#   data.plot$y <- data.plot[, x2]
-#   data.plot$pt.size <- pt.size
-#   names(x = data.plot) <- c("x", "y")
-#   data.use <- t(x = FetchData(object = object, vars.all = features.plot, 
-#                               cells.use = cells.use, use.imputed = use.imputed))
-#   min.cutoff <- mapply(FUN = function(cutoff, feature) {
-#     ifelse(test = is.na(x = cutoff), yes = min(data.use[feature, 
-#                                                         ]), no = cutoff)
-#   }, cutoff = min.cutoff, feature = features.plot)
-#   max.cutoff <- mapply(FUN = function(cutoff, feature) {
-#     ifelse(test = is.na(x = cutoff), yes = max(data.use[feature, 
-#                                                         ]), no = cutoff)
-#   }, cutoff = max.cutoff, feature = features.plot)
-#   check_lengths = unique(x = vapply(X = list(features.plot, 
-#                                              min.cutoff, max.cutoff), FUN = length, FUN.VALUE = numeric(length = 1)))
-#   if (length(x = check_lengths) != 1) {
-#     stop("There must be the same number of minimum and maximum cuttoffs as there are features")
-#   }
-#   if (overlay) {
-#     pList <- list(BlendPlot(data.use = data.use, features.plot = features.plot, 
-#                             data.plot = data.plot, pt.size = pt.size, pch.use = pch.use, 
-#                             cols.use = cols.use, dim.codes = dim.codes, min.cutoff = min.cutoff, 
-#                             max.cutoff = max.cutoff, coord.fixed = coord.fixed, 
-#                             no.axes = no.axes, no.legend = no.legend, dark.theme = dark.theme))
-#   }
-#   else {
-#     pList <- mapply(FUN = SingleFeaturePlot, feature = features.plot, 
-#                     min.cutoff = min.cutoff, max.cutoff = max.cutoff, 
-#                     coord.fixed = coord.fixed, MoreArgs = list(data.use = data.use, 
-#                                                                data.plot = data.plot, pt.size = pt.size, pch.use = pch.use, 
-#                                                                cols.use = cols.use, dim.codes = dim.codes, no.axes = no.axes, 
-#                                                                no.legend = no.legend, dark.theme = dark.theme, 
-#                                                                vector.friendly = vector.friendly, png.file = png.file, 
-#                                                                png.arguments = png.arguments), SIMPLIFY = FALSE)
-#   }
-#   if (do.hover) {
-#     if (length(x = pList) != 1) {
-#       stop("'do.hover' only works on a single feature or an overlayed FeaturePlot")
-#     }
-#     if (is.null(x = data.hover)) {
-#       features.info <- NULL
-#     }
-#     else {
-#       features.info <- FetchData(object = object, vars.all = data.hover)
-#     }
-#     return(HoverLocator(plot = pList[[1]], data.plot = data.plot, 
-#                         features.info = features.info, dark.theme = dark.theme, 
-#                         title = features.plot))
-#   }
-#   else if (do.identify) {
-#     if (length(x = pList) != 1) {
-#       stop("'do.identify' only works on a single feature or an overlayed FeaturePlot")
-#     }
-#     return(FeatureLocator(plot = pList[[1]], data.plot = data.plot, 
-#                           dark.theme = dark.theme))
-#   }
-#   else {
-#     print(x = cowplot::plot_grid(plotlist = pList, ncol = nCol))
-#   }
-#   ResetPar()
-#   if (do.return) {
-#     return(pList)
-#   }
-# }
-# 
+ls2<- foreach(i=1:l(ls2)) %dopar% {
+  FilterCells(object = ls2[[i]], subset.names = c("nGene", "percent.mito", "percent.ribo"),
+              low.thresholds = c(p$thr.hp.nGene, -Inf, -Inf),
+              high.thresholds = c(p$thr.lp.nGene, p$thr.lp.mito, p$thr.lp.ribo))
+}; say()
+l(ls2); ls2
+
+
+# NormalizeData ------------------------------
+ls2<- foreach(i=1:l(ls2)) %dopar% {
+  NormalizeData(object = ls2[[i]], normalization.method = "LogNormalize", scale.factor = 10000)
+}; say()
+l(ls2); ls20
+
+# FindVariableGenes cannot be parallelized sadly   ------------------------------
+start_time <- Sys.time()
+for (i in 1:l(inNames)) { print(i)
+  ls2[[i]]  <- FindVariableGenes(object = ls2[[i]], mean.function = ExpMean, dispersion.function = LogVMR,
+                                 x.low.cutoff = 0.0125, x.high.cutoff = 3, y.cutoff = 0.5 )
+  wplot_save_this(plotname = ppp("Var.genes.",inNames[i]), PNG = T)   
+}; say()
+end_time <- Sys.time(); end_time - start_time
+
+# ScaleData is parallelizedialready  ------------------------------
+for (i in 1:l(inNames)) { print(i)
+  ls2[[i]]  <- ScaleData(object = ls2[[i]], vars.to.regress = ls.vars2regress[[i]], do.par = T, num.cores = 6)
+}; say()
+
+
