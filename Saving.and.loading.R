527b8e72fa6b3c6c6e8c744d6565f1b23060224d
Abel Vertesy <vertesy@users.noreply.github.com>
2019-12-09 20:11:16 +0100

    Many saving func.
    sampleNpc
    seuSaveRds
    subsetSeuObj.and.Save
    isave.image
    etc

--- Saving.and.loading.R
+++ Saving.and.loading.R
@@ -2,30 +2,93 @@
 # Modified from: https://rpubs.com/jeffjjohnston/rds_compression 
 
 
+
+# Save an object -----------------------------------------------
+isave.RDS <- function(object, prefix =NULL, suffix=NULL, showMemObject=T){ # faster saving of workspace, and compression outside R, when it can run in the background. Seemingly quite CPU hungry and not veryefficient compression.
+  path_rdata = paste0("~/Documents/RDS.files/", basename(OutDir))
+  dir.create(path_rdata)
+  
+  if (showMemObject) { memory.biggest.objects() }
+  fnameBase = kppu(prefix, substitute(object), suffix, idate())
+  fname = MarkdownReportsDev::kollapse(path_rdata, "/",fnameBase , ".Rds")
+  tictoc::tic()
+  saveRDS(object, file = fname, compress=F)
+  tictoc::toc()
+  MarkdownReportsDev::iprint("Saved, being compressed", fname)
+  say()
+  system(paste("gzip", fname),  wait = FALSE) # execute in the background
+}
+
+
+# Save an object -----------------------------------------------
+isave.RDS.pigz <- function(object, prefix =NULL, suffix=NULL, showMemObject=T){ # faster saving of workspace, and compression outside R, when it can run in the background. Seemingly quite CPU hungry and not veryefficient compression.
+  path_rdata = paste0("~/Documents/RDS.files/", basename(OutDir))
+  dir.create(path_rdata)
+  
+  if (showMemObject) { memory.biggest.objects() }
+  fnameBase = kppu(prefix, substitute(object), suffix, idate())
+  fname = MarkdownReportsDev::kollapse(path_rdata, "/",fnameBase , ".Rds")
+  tictoc::tic()
+  ssaveRDS(object = object, file = fname)
+  tictoc::toc()
+  say()
+}
+
+
 # Save workspace -----------------------------------------------
 # requires MarkdownReportsDev (github) and defining OutDir
 # requires github/vertesy/CodeAndRoll.r 
 
-
-isave <- function(..., showMemObject=T){ # faster saving of workspace, and compression outside R, when it can run in the background. Seemingly quite CPU hungry and not veryefficient compression.
+isave.image <- function(..., showMemObject=T, options=c("--force", NULL)[1]){ # faster saving of workspace, and compression outside R, when it can run in the background. Seemingly quite CPU hungry and not veryefficient compression.
   path_rdata = paste0("~/Documents/Rdata.files/", basename(OutDir))
   dir.create(path_rdata)
   
   if (showMemObject) { memory.biggest.objects() }
   fname = MarkdownReportsDev::kollapse(path_rdata, "/",idate(),...,".Rdata")
+  print(fname)
+  if (nchar(fname) > 2000) stop()
   save.image( file = fname, compress=F)
   MarkdownReportsDev::iprint("Saved, being compressed", fname)
-  system(paste("gzip", fname),  wait = FALSE) # execute in the background
+  system(paste("gzip", options, fname),  wait = FALSE) # execute in the background
+}
+
+# ------------------------------------------------------------------------
+
+subsetSeuObj.and.Save <- function(obj=ORC, fraction = 0.25 ) {
+  cellIDs.keep = sampleNpc(metaDF = obj@meta.data, pc = fraction)
+  
+  obj_Xpc <- subset(obj, cells = cellIDs.keep) # downsample
+  saveRDS(obj_Xpc, compress = TRUE,
+          file = ppp(p0(InputDir, 'seu.ORC'), l(cellIDs.keep), 'cells.with.min.features', p$min.features,"Rds" ) )
+  say()
 }
 
 
+# ------------------------------------------------------------------------
+
+seuSaveRds <- function(object = ls.Seurat, tags = setupFlags, use_Original_OutDir = F) {
+  if (use_Original_OutDir) create_set_Original_OutDir()
+  fname.comb.rds = ppp(substitute(object), tags, idate(), ".Rds")
+  iprint( fname.comb.rds)
+  ssaveRDS(object = object, filename = fname.comb.rds)
+  say()
+}
+
+# ------------------------------------------------------------------------
+
+sampleNpc <- function(metaDF = MetaData[which(Pass),], pc=0.1) {
+  cellIDs = rownames(metaDF)
+  nr_cells = floor(l(cellIDs) * pc)
+  cellIDs.keep = sample(cellIDs, size = nr_cells, replace = FALSE)
+  return(cellIDs.keep)
+}
 
 # Wrapper layer 2 (top) -----------------------------------------------------------------------------------
 
 # read / load multiple objects 
 rrRDS <- function(list_of_objectnames = c("ls.Seurat", "ls2", "org"), ...) { # load a list of RDS files with parallel unzip pgzip
   tictoc::tic()
-  path_rdata = paste0("~/Documents/Rdata.files/", basename(OutDir))
+  path_rdata = paste0("~/Documents/RDS.files/", basename(OutDir))
   iprint("Looking for files under:" , path_rdata)
   iprint("(Base on OutDir)")
   dir.exists(path_rdata)
@@ -54,10 +117,10 @@ rrRDS <- function(list_of_objectnames = c("ls.Seurat", "ls2", "org"), ...) { # l
 sssRDS <- function(list_of_objectnames = c("ls.Seurat", "ls2", "org.ALL", "org"), name.suffix =NULL, ...) { # parallel save RDS
   tictoc::tic()
   base_name <- character()
-  path_rdata = paste0("~/Documents/Rdata.files/", basename(OutDir))
+  path_rdata = paste0("~/Documents/RDS.files/", basename(OutDir))
   iprint("Files saved under:" , path_rdata)
   
-  dir.create(path_rdata)
+  try(dir.create(path_rdata))
   for (obj in list_of_objectnames) {
     print(obj)
     if ( "seurat" %in% is(obj)) { 
@@ -68,13 +131,14 @@ sssRDS <- function(list_of_objectnames = c("ls.Seurat", "ls2", "org.ALL", "org")
     fname = paste0(path_rdata , "/", base_name[i], collapse = "")
     ssaveRDS( object = get(obj), filename = fname, ...)
   }
-  dput(base_name)
   tictoc::toc()
+  dput(base_name)
 }
 
 # Wrapper layer 1 -----------------------------------------------------------------------------------
 ssaveRDS <- function(object, filename, con_func = list(pigz_pipe, snappy_pipe)[[1]], 
                      func_type = c("pipe", "builtin")[1], ...) {
+  tictoc::tic()
   if(func_type == "builtin") {
     con <- con_func(filename)
   } else {
@@ -82,17 +146,21 @@ ssaveRDS <- function(object, filename, con_func = list(pigz_pipe, snappy_pipe)[[
   }
   on.exit(close(con))
   saveRDS(object, con)
+  tictoc::toc()
 }
 
 rreadRDS <- function(filename, con_func = list(pigz_pipe, snappy_pipe)[[1]], 
                      func_type = c("pipe", "builtin")[1], ...) {
+  tictoc::tic()
   if(func_type == "builtin") {
     con <- con_func(filename)
   } else {
     con <- con_func(filename, mode="read", ...)
   }
   on.exit(close(con))
-  readRDS(con)
+  obj=readRDS(con)
+  tictoc::toc()
+  return(obj)
 }
 
 # Pipes -----------------------------------------------------------------------------------
