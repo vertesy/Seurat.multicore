5e697a81186a3cbb604d671f02fb187d594b11af
Abel Vertesy <vertesy@users.noreply.github.com>
2020-10-02 10:52:49 +0200

    Load directly from GitHub

---
+++ Seurat3.Multicore.Load.from.GitHub.R
@@ -0,0 +1,14 @@
+######################################################################
+# Seurat3.Multicore.Load.from.GitHub.R
+######################################################################
+# try(source ('https://raw.githubusercontent.com/vertesy/Seurat.multicore/master/Seurat3.Multicore.Load.from.GitHub.R'), silent=T)
+
+### Seurat 3 ------------------------
+try(source("https://raw.githubusercontent.com/vertesy/Seurat.multicore/master/Seurat3.Multicore.Read.Write.R"))
+try(source("https://raw.githubusercontent.com/vertesy/Seurat.multicore/master/Seurat3.Multicore.Generic.Functions.R"))
+# try(source("https://raw.githubusercontent.com/vertesy/Seurat.multicore/master/Seurat.Functions.other.R"))
+
+
+### Seurat 2 ------------------------
+# try(source("https://raw.githubusercontent.com/vertesy/Seurat.multicore/master/Seurat2.Multicore.Functions.R"))
+
