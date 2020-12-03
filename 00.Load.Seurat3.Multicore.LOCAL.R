2ffbb628ce173121e4048622d3b09c9ed7895370
Abel Vertesy <vertesy@users.noreply.github.com>
2020-12-03 12:01:38 +0100

    rename cleanup

---
+++ 00.Load.Seurat3.Multicore.LOCAL.R
@@ -0,0 +1,14 @@
+######################################################################
+# 00.Load.Seurat3.Multicore.LOCAL.R
+######################################################################
+# source('~/GitHub/Packages/Seurat.multicore/00.Load.Seurat3.Multicore.LOCAL.R')
+
+### Seurat 3 ------------------------
+try(source("~/GitHub/Packages/Seurat.multicore/Functions/Seurat3.Multicore.Read.Write.R"))
+try(source("~/GitHub/Packages/Seurat.multicore/Functions/Seurat3.Multicore.Generic.Functions.R"))
+# try(source("~/GitHub/Packages/Seurat.multicore/Functions/Seurat.Functions.other.R"))
+
+
+### Seurat 2 ------------------------
+# try(source("~/GitHub/Packages/Seurat.multicore/Seurat2.Multicore.Functions.R"))
+
--- 00.Load.Seurat3.Multicore.WEB.R
+++ 00.Load.Seurat3.Multicore.WEB.R
@@ -1,10 +1,10 @@
 ######################################################################
-# Seurat3.Multicore.Load.from.GitHub.R
+# 00.Load.Seurat3.Multicore.WEB.R
 ######################################################################
-# try(source('https://raw.githubusercontent.com/vertesy/Seurat.multicore/master/Seurat3.Multicore.Load.from.GitHub.R'), silent=T)
+# try(source('https://raw.githubusercontent.com/vertesy/Seurat.multicore/master/00.Load.Seurat3.Multicore.WEB.R'), silent=T)
 
 ### Seurat 3 ------------------------
-try(source("https://raw.githubusercontent.com/vertesy/Seurat.multicore/master/Seurat3.Multicore.Read.Write.R"))
+try(source("https://raw.githubusercontent.com/vertesy/Seurat.multicore/master/Functions/Seurat3.Multicore.Read.Write.R"))
 try(source("https://raw.githubusercontent.com/vertesy/Seurat.multicore/master/Seurat3.Multicore.Generic.Functions.R"))
 # try(source("https://raw.githubusercontent.com/vertesy/Seurat.multicore/master/Seurat.Functions.other.R"))
 
